add282101079e78020cc1ebd1cd080017c2d89b2
Matthew Chung <matthewchung74@gmail.com>
2014-11-10 11:02:36 -0800

    added xs version redirect www.testerurl.com to testerurl.com

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -1,7 +1,20 @@
 'use strict';
 
 module.exports = function (app) {
-    // Root routing
+
+    // based on http://stackoverflow.com/questions/17898183/node-js-301-redirect-non-www-without-express
+    // and redirecting as 301 for SEO
+    app.get ('/*', function (req, res, next){
+        if (req.headers.host.match(/^www\.testerurl\.com/)) {
+            res.writeHead (301, {'Location': 'http://testerurl.com'});
+            res.end();
+            return;
+        }
+        else {
+            return next();
+        }
+    } );
+
     var core = require('../../app/controllers/core');
     app.route('/').get(core.index);
 };
